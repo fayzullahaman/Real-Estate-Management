1ed3a5331146dcbaa91c0f951aceb0dea9af4308
MD. FAYZULLAH AMAN <fayzullahaman@gmail.com>
2023-01-06 23:08:24 +0600

    some customise frontend react js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,163 +2,159 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <>
-      {/* <!-- Footer Start --> */}
-      <div
-        className="container-fluid bg-dark text-white-50 footer pt-5 mt-5 wow fadeIn"
-        data-wow-delay="0.1s"
-      >
-        <div className="container py-5">
-          <div className="row g-5">
-            <div className="col-lg-3 col-md-6">
-              <h5 className="text-white mb-4">Get In Touch</h5>
-              <p className="mb-2">
-                <i className="fa fa-map-marker-alt me-3"></i>Scout Market,
-                Kakrail, Dhaka
-              </p>
-              <p className="mb-2">
-                <i className="fa fa-phone-alt me-3"></i>+012 345 67890
-              </p>
-              <p className="mb-2">
-                <i className="fa fa-envelope me-3"></i>realstate@example.com
-              </p>
-              <div className="d-flex pt-2">
-                <a className="btn btn-outline-light btn-social" href="">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a className="btn btn-outline-light btn-social" href="">
-                  <i className="fab fa-facebook-f"></i>
-                </a>
-                <a className="btn btn-outline-light btn-social" href="">
-                  <i className="fab fa-youtube"></i>
-                </a>
-                <a className="btn btn-outline-light btn-social" href="">
-                  <i className="fab fa-linkedin-in"></i>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <h5 className="text-white mb-4">Quick Links</h5>
-              <a className="btn btn-link text-white-50" href="">
-                About Us
+    <div
+      className="container-fluid bg-dark text-white-50 footer pt-5 mt-5 wow fadeIn"
+      data-wow-delay="0.1s"
+    >
+      <div className="container py-5">
+        <div className="row g-5">
+          <div className="col-lg-3 col-md-6">
+            <h5 className="text-white mb-4">Get In Touch</h5>
+            <p className="mb-2">
+              <i className="fa fa-map-marker-alt me-3"></i>Scout Market,
+              Kakrail, Dhaka
+            </p>
+            <p className="mb-2">
+              <i className="fa fa-phone-alt me-3"></i>+012 345 67890
+            </p>
+            <p className="mb-2">
+              <i className="fa fa-envelope me-3"></i>realestate@example.com
+            </p>
+            <div className="d-flex pt-2">
+              <a className="btn btn-outline-light btn-social" href="">
+                <i className="fab fa-twitter"></i>
               </a>
-              <a className="btn btn-link text-white-50" href="">
-                Contact Us
+              <a className="btn btn-outline-light btn-social" href="">
+                <i className="fab fa-facebook-f"></i>
               </a>
-              <a className="btn btn-link text-white-50" href="">
-                Our Services
+              <a className="btn btn-outline-light btn-social" href="">
+                <i className="fab fa-youtube"></i>
               </a>
-              <a className="btn btn-link text-white-50" href="">
-                Privacy Policy
-              </a>
-              <a className="btn btn-link text-white-50" href="">
-                Terms & Condition
+              <a className="btn btn-outline-light btn-social" href="">
+                <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
-            <div className="col-lg-3 col-md-6">
-              <h5 className="text-white mb-4">Photo Gallery</h5>
-              <div className="row g-2 pt-2">
-                <div className="col-4">
-                  <img
-                    className="img-fluid rounded bg-light p-1"
-                    src="/assets/img/property-1.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="col-4">
-                  <img
-                    className="img-fluid rounded bg-light p-1"
-                    src="/assets/img/property-2.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="col-4">
-                  <img
-                    className="img-fluid rounded bg-light p-1"
-                    src="/assets/img/property-3.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="col-4">
-                  <img
-                    className="img-fluid rounded bg-light p-1"
-                    src="/assets/img/property-4.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="col-4">
-                  <img
-                    className="img-fluid rounded bg-light p-1"
-                    src="/assets/img/property-5.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="col-4">
-                  <img
-                    className="img-fluid rounded bg-light p-1"
-                    src="/assets/img/property-6.jpg"
-                    alt=""
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <h5 className="text-white mb-4">Newsletter</h5>
-              <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-              <div
-                className="position-relative mx-auto"
-                style={{ maxWidth: "400px" }}
-              >
-                <input
-                  className="form-control bg-transparent w-100 py-3 ps-4 pe-5"
-                  type="text"
-                  placeholder="Your email"
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <h5 className="text-white mb-4">Quick Links</h5>
+            <a className="btn btn-link text-white-50" href="">
+              About Us
+            </a>
+            <a className="btn btn-link text-white-50" href="">
+              Contact Us
+            </a>
+            <a className="btn btn-link text-white-50" href="">
+              Our Services
+            </a>
+            <a className="btn btn-link text-white-50" href="">
+              Privacy Policy
+            </a>
+            <a className="btn btn-link text-white-50" href="">
+              Terms & Condition
+            </a>
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <h5 className="text-white mb-4">Photo Gallery</h5>
+            <div className="row g-2 pt-2">
+              <div className="col-4">
+                <img
+                  className="img-fluid rounded bg-light p-1"
+                  src="/assets/img/property-1.jpg"
+                  alt=""
                 />
-                <button
-                  type="button"
-                  className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2"
-                >
-                  SignUp
-                </button>
+              </div>
+              <div className="col-4">
+                <img
+                  className="img-fluid rounded bg-light p-1"
+                  src="/assets/img/property-2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="col-4">
+                <img
+                  className="img-fluid rounded bg-light p-1"
+                  src="/assets/img/property-3.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="col-4">
+                <img
+                  className="img-fluid rounded bg-light p-1"
+                  src="/assets/img/property-4.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="col-4">
+                <img
+                  className="img-fluid rounded bg-light p-1"
+                  src="/assets/img/property-5.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="col-4">
+                <img
+                  className="img-fluid rounded bg-light p-1"
+                  src="/assets/img/property-6.jpg"
+                  alt=""
+                />
               </div>
             </div>
           </div>
+          <div className="col-lg-3 col-md-6">
+            <h5 className="text-white mb-4">Newsletter</h5>
+            <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+            <div
+              className="position-relative mx-auto"
+              style={{ maxWidth: "400px" }}
+            >
+              <input
+                className="form-control bg-transparent w-100 py-3 ps-4 pe-5"
+                type="text"
+                placeholder="Your email"
+              />
+              <button
+                type="button"
+                className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2"
+              >
+                SignUp
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="container">
-          <div className="copyright">
-            <div className="row">
-              <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                &copy;{" "}
-                <a
-                  className="border-bottom"
-                  href="https://realstate.bdprogrammers.com/"
-                >
-                  Real State
-                </a>
-                , All Right Reserved.
-                {/* This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. */}
-                Designed By
-                <a
-                  className="border-bottom"
-                  href="https://fayzullahaman.bdprogrammers.com/ "
-                >
-                  {" "}
-                  fayzullahaman
-                </a>
-              </div>
-              <div className="col-md-6 text-center text-md-end">
-                <div className="footer-menu">
-                  <a href="">Home</a>
-                  <a href="">Cookies</a>
-                  <a href="">Help</a>
-                  <a href="">FQAs</a>
-                </div>
+      </div>
+      <div className="container">
+        <div className="copyright">
+          <div className="row">
+            <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+              &copy;{" "}
+              <a
+                className="border-bottom"
+                href="https://realstate.bdprogrammers.com/"
+              >
+                Real Estate
+              </a>
+              , All Right Reserved.
+              {/* This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. */}
+              Designed By
+              <a
+                className="border-bottom"
+                href="https://fayzullahaman.bdprogrammers.com/ "
+              >
+                {" "}
+                fayzullahaman
+              </a>
+            </div>
+            <div className="col-md-6 text-center text-md-end">
+              <div className="footer-menu">
+                <a href="">Home</a>
+                <a href="">Cookies</a>
+                <a href="">Help</a>
+                <a href="">FQAs</a>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* <!-- Footer End --> */}
-    </>
+    </div>
   );
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -45,29 +45,176 @@ export default function Home() {
               </p>
             </div>
             <div className="row g-4">
-              {property.map((proper, index) => (
-                <div
-                  className="col-lg-3 col-sm-6 wow fadeInUp"
-                  data-wow-delay="0.1s"
+              {/* {property.map((proper, index) => ( */}
+              <div
+                className="col-lg-3 col-sm-6 wow fadeInUp"
+                data-wow-delay="0.1s"
+              >
+                <a
+                  className="cat-item d-block bg-light text-center rounded p-3"
+                  href=""
                 >
-                  <a
-                    className="cat-item d-block bg-light text-center rounded p-3"
-                    href=""
-                  >
-                    <div className="rounded p-4">
-                      <div className="icon mb-3">
-                        <img
-                          className="img-fluid"
-                          src="/assets/img/icon-apartment.png"
-                          alt="Icon"
-                        />
-                      </div>
-                      <h6>Apartment</h6>
-                      <span>123 Properties</span>
+                  <div className="rounded p-4">
+                    <div className="icon mb-3">
+                      <img
+                        className="img-fluid"
+                        src="/assets/img/icon-apartment.png"
+                        alt="Icon"
+                      />
                     </div>
-                  </a>
-                </div>
-              ))}
+                    <h6>Apartment</h6>
+                    <span>123 Properties</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                className="col-lg-3 col-sm-6 wow fadeInUp"
+                data-wow-delay="0.3s"
+              >
+                <a
+                  className="cat-item d-block bg-light text-center rounded p-3"
+                  href=""
+                >
+                  <div className="rounded p-4">
+                    <div className="icon mb-3">
+                      <img
+                        className="img-fluid"
+                        src="/assets/img/icon-villa.png"
+                        alt="Icon"
+                      />
+                    </div>
+                    <h6>Villa</h6>
+                    <span>123 Properties</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                className="col-lg-3 col-sm-6 wow fadeInUp"
+                data-wow-delay="0.5s"
+              >
+                <a
+                  className="cat-item d-block bg-light text-center rounded p-3"
+                  href=""
+                >
+                  <div className="rounded p-4">
+                    <div className="icon mb-3">
+                      <img
+                        className="img-fluid"
+                        src="/assets/img/icon-house.png"
+                        alt="Icon"
+                      />
+                    </div>
+                    <h6>Home</h6>
+                    <span>123 Properties</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                className="col-lg-3 col-sm-6 wow fadeInUp"
+                data-wow-delay="0.7s"
+              >
+                <a
+                  className="cat-item d-block bg-light text-center rounded p-3"
+                  href=""
+                >
+                  <div className="rounded p-4">
+                    <div className="icon mb-3">
+                      <img
+                        className="img-fluid"
+                        src="/assets/img/icon-housing.png"
+                        alt="Icon"
+                      />
+                    </div>
+                    <h6>Office</h6>
+                    <span>123 Properties</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                className="col-lg-3 col-sm-6 wow fadeInUp"
+                data-wow-delay="0.1s"
+              >
+                <a
+                  className="cat-item d-block bg-light text-center rounded p-3"
+                  href=""
+                >
+                  <div className="rounded p-4">
+                    <div className="icon mb-3">
+                      <img
+                        className="img-fluid"
+                        src="/assets/img/icon-building.png"
+                        alt="Icon"
+                      />
+                    </div>
+                    <h6>Building</h6>
+                    <span>123 Properties</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                className="col-lg-3 col-sm-6 wow fadeInUp"
+                data-wow-delay="0.3s"
+              >
+                <a
+                  className="cat-item d-block bg-light text-center rounded p-3"
+                  href=""
+                >
+                  <div className="rounded p-4">
+                    <div className="icon mb-3">
+                      <img
+                        className="img-fluid"
+                        src="/assets/img/icon-neighborhood.png"
+                        alt="Icon"
+                      />
+                    </div>
+                    <h6>Townhouse</h6>
+                    <span>123 Properties</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                className="col-lg-3 col-sm-6 wow fadeInUp"
+                data-wow-delay="0.5s"
+              >
+                <a
+                  className="cat-item d-block bg-light text-center rounded p-3"
+                  href=""
+                >
+                  <div className="rounded p-4">
+                    <div className="icon mb-3">
+                      <img
+                        className="img-fluid"
+                        src="/assets/img/icon-condominium.png"
+                        alt="Icon"
+                      />
+                    </div>
+                    <h6>Shop</h6>
+                    <span>123 Properties</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                className="col-lg-3 col-sm-6 wow fadeInUp"
+                data-wow-delay="0.7s"
+              >
+                <a
+                  className="cat-item d-block bg-light text-center rounded p-3"
+                  href=""
+                >
+                  <div className="rounded p-4">
+                    <div className="icon mb-3">
+                      <img
+                        className="img-fluid"
+                        src="/assets/img/icon-luxury.png"
+                        alt="Icon"
+                      />
+                    </div>
+                    <h6>Garage</h6>
+                    <span>123 Properties</span>
+                  </div>
+                </a>
+              </div>
+              {/* ))} */}
             </div>
           </div>
         </div>
